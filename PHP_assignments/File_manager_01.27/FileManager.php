160f91e72d6bb4702f8d9582206898baa35e0784
MantasKub <mkubiliunas@gmail.com>
2023-01-31 17:03:17 +0200

    Updated

--- PHP_assignments/File_manager_01.27/FileManager.php
+++ PHP_assignments/File_manager_01.27/FileManager.php
@@ -1,9 +1,13 @@
 
 <?php 
 
+
+//Pagrindine direktorija, kurioje patalpintas sis failas
 $dir = './';
 $back_link = '';
 
+
+//Atgalines nuorodos generavimas
 if(isset($_GET['dir']) AND $_GET['dir'] != '') {
   $dir = $_GET['dir'];
 
@@ -18,11 +22,13 @@ if(isset($_GET['dir']) AND $_GET['dir'] != '') {
     }
     $back_link = '
       <tr>
-        <td><button class="btn btn-primary"><a href="?dir=' . $back_link . '">Back</button></td>
+        <td colspan="2"><a class="btn btn-primary" href="?dir=' . $back_link . '">Back</a></td>
       </tr>';
   }
 }
 
+
+//Naujo failo arba folderio pridejimas
 if(isset($_POST['data_type']) AND $_POST['data_type'] === '1') {
   if(isset($_POST['folder_name']) AND $_POST['folder_name'] != '') {
     mkdir($dir . '/' . $_POST['folder_name']);
@@ -36,14 +42,49 @@ if(isset($_POST['data_type']) AND $_POST['data_type'] === '1') {
   }
 }
 
+
+//Failo pavadinimo keitimas
+if(isset($_POST['file_name_edited']) AND $_POST['file_name_edited'] != '') {
+  $file_path = explode('/', $_GET['edit']);
+  unset($file_path[count($file_path) - 1]);
+  $file_path[] = $_POST['file_name_edited'];
+
+  $to = implode('/', $file_path);
+
+
+  //Pavadinimo redagavimo eilute panaudojant basename funkcija
+  // $newFile = str_replace(basename($_GET['edit']), $_POST['file_name_edited'], $_GET['edit']);
+
+  rename($_GET['edit'], $to);
+
+  header('Location: ?dir=' . $dir);
+}
+
+
+//Failu ir direktoriju istrynimas
+if(isset($_GET['delete']) AND $_GET['delete'] !='') {
+  if($_GET['delete'] === basename(__FILE__)) {
+      header('Location: ?dir=' . $dir . '&m=Cannot delete main file');
+} else {
+
+  unlink($_GET['delete']);
+  header('Location: ?dir=' . $dir);
+  }
+}
+
+if(isset($_FILES['file_upload']) AND count($_FILES['file_upload']) > 0) {
+
+    $tmpFile = $_FILES['file_upload']['tmp_name'];
+    $target = $dir === './' ? $_FILES['file_upload']['name'] : $dir . '/' . $_FILES['file_upload']['name'];
+    
+    move_uploaded_file($tmpFile, $target);
+    header('Location: ' . $_SERVER['REQUEST_URI']);
+}
+
 $data = scandir($dir);
 
 unset($data[0]);
 unset($data[1]);
-
-
-
-
 
 ?>
 
@@ -58,14 +99,22 @@ unset($data[1]);
   <title>File Manager</title>
 </head>
 <body>
-  <div class="container">
+  <div class="container py-4">
+    <?php if(isset($_GET['m']) AND $_GET['m'] != '') { ?>
+      <div class="alert alert-danger">
+        <?= $_GET['m'] ?>
+      </div>
+    <?php } ?>
+
       <table class="table">
         <thead>
           <tr>
             <th>Name</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
+          <!--Failu ir folderiu sarasas -->
           <?php 
             foreach($data as $item) {
 
@@ -76,7 +125,9 @@ unset($data[1]);
                 $path = $dir . '/' . $item;
               }
 
-              $icon = 'folder';
+              //Ikonu priskirimas pagal failo tipa
+
+              $icon = 'folder-fill';
 
               $file_formats = [
                 'pdf' => 'file-earmark-pdf', 
@@ -98,70 +149,122 @@ unset($data[1]);
               $filename = explode('.', $item);
               $filename = $filename[count($filename) - 1];
 
-              if(array_key_exists('$filename', $file_formats)) {
+              if(array_key_exists($filename, $file_formats)) {
                 $icon = $file_formats[$filename];
               }
-
-              
             }
-
             ?>
               <tr>
                 <td>
                   <i class="bi bi-<?= $icon ?>"></i>
-                  <?= (is_dir($path)) ? '<a href="?dir=' . $path . '">' . $item . '</a>' : $item ?>
+                  <?php 
+                  if(is_dir($path)) {
+                    echo '<a href="?dir=' . $path . '">' . $item . '</a>';
+                   } else { 
+                    echo '<a href="'.$path.'" target="_blank">' . $item . '</a>';
+                   }
+                   ?>
+                   
+                </td>
+                <td>
+                  <a href="?edit=<?= $path ?>&dir=<?= $dir ?>" class="btn btn-success">Edit</a> 
+                  <a href="?delete=<?= $path ?>&dir=<?= $dir ?>" class="btn btn-danger">Delete</a> 
                 </td>
               </tr>
             <?php } ?>
+            <!--Atgalines nuorodos atvaizdavimas -->
             <?php echo $back_link; ?>
         </tbody>
       </table>
 
-      <h2>Create new file or Folder</h2>
 
-      <form method="POST">
-      <div class="mb-3">
-        <label>Select data type</label>
-        <select name="data_type" class="form-control">
-          <option value="1">Folder</option>
-          <option value="2">File</option>
-        </select>
-      </div>
-      <div class="folder">
+      <!--Failo pavadinimo redagavimo forma -->
+      <?php if(isset($_GET['edit'])) { ?>
+
+        <h2>Edit file name</h2>
+
+        <form method="POST">
+              <!-- <input type="hidden" name="file_name_original" class="form-control" value="<?= $_GET['edit'] ?>"/> -->
+          <div class="mb-3">
+              <label>New file name</label>
+              <input type="text" name="file_name_edited" class="form-control"/>
+          </div>
+          <button class="btn btn-primary">Submit</button>
+        </form>
+
+        <?php } else { ?>
+
+
+      <!--Naujo failo arba folderio priedejimo forma -->
+      <h2>Create new file or Folder</h2>
+      <!--encytype3="multipart/form-data" - atributas igalina failu persiuntima per html forma-->
+      <form method="POST" enctype="multipart/form-data">
         <div class="mb-3">
-            <label>Folder name</label>
-            <input type="text" name="folder_name" class="form-control"/>
+          <label>Select data type</label>
+          <select name="data_type" class="form-control">
+            <option value="1">Folder</option>
+            <option value="2">File</option>
+            <option value="3">Upload</option>
+          </select>
         </div>
-      </div>
-      <div class="file">
-        <div class="mb-3">
-          <label>File name</label>
-          <input type="text" name="file_name" class="form-control"/>
+        <div class="folder">
+          <div class="mb-3">
+              <label>Folder name</label>
+              <input type="text" name="folder_name" class="form-control"/>
+          </div>
         </div>
-        <div class="mb-3">
-          <label>File contents</label>
-          <textarea name="file_contents" class="form-control"></textarea>
+        <div class="file">
+          <div class="mb-3">
+            <label>File name</label>
+            <input type="text" name="file_name" class="form-control"/>
+          </div>
+          <div class="mb-3">
+            <label>File contents</label>
+            <textarea name="file_contents" class="form-control"></textarea>
+          </div>
         </div>
-      </div>
+        <div class="upload">
+          <div class="mb-3">
+              <label>Select file to upload</label>
+              <input type="file" name="file_upload" class="form-control"/>
+          </div>
+        </div>
         <div class="mb-3">
           <button class="btn btn-primary">Add</button>
         </div>
       </form>
+
+
+      <!--Formu pasikeitimai-->
+      <script>
+        document.querySelector('.file').style.display = 'none';
+        document.querySelector('.upload').style.display = 'none';
+
+        document.querySelector('[name="data_type"]').addEventListener('change', (e) => {
+          if(event.target.value === '1') {
+            document.querySelector('.file').style.display = 'none';
+            document.querySelector('.upload').style.display = 'none';
+            document.querySelector('.folder').style.display = 'block';
+
+          } else if (e.target.value === '3') {
+            
+            document.querySelector('.file').style.display = 'none';
+            document.querySelector('.folder').style.display = 'none';
+            document.querySelector('.upload').style.display = 'block';
+
+          } else {
+            document.querySelector('.file').style.display = 'block';
+            document.querySelector('.folder').style.display = 'none';
+            document.querySelector('.upload').style.display = 'none';
+          }
+        });
+
+    </script>
+
+
+      <?php } ?>
     </div>
     
-    <script>
-      document.querySelector('.file').style.display = 'none';
-
-      document.querySelector('[name="data_type"]').addEventListener('change', (e) => {
-        if(event.target.value === '1') {
-          document.querySelector('.file').style.display = 'none';
-          document.querySelector('.folder').style.display = 'block';
-        } else {
-          document.querySelector('.file').style.display = 'block';
-          document.querySelector('.folder').style.display = 'none';
-        }
-      });
-    </script>
   
 </body>
 </html>
