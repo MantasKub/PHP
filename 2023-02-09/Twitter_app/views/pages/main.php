b6a3a38ab204b23b41d9e0f9138e11f36c5bbf8d
MantasKub <mkubiliunas@gmail.com>
2023-02-09 18:16:10 +0200

    Simple social network app

---
+++ 2023-02-09/Twitter_app/views/pages/main.php
@@ -0,0 +1,9 @@
+<?php 
+  if(!isset($_SESSION['user'])) {
+    header('Location: ?page=login');
+    exit;
+  }
+?>
+
+<h1>Latest tweets</h1>
+
---
+++ 2023-02-09/Twitter_app/views/pages/register.php
@@ -0,0 +1,75 @@
+
+<?php 
+  if(!empty($_POST)) {
+
+    $data = [];
+
+    if(file_exists('./database.json'))
+      $data = json_decode(file_get_contents('./database.json'), true);
+
+    $emailExists = array_filter($data, function($user) {
+      if($user['email'] === $_POST['email'])
+        return true;
+
+        return false;
+    });
+
+    $params = [
+      'page' => 'register',
+      'message' => 'This ID is already taken',
+      'status' => 'danger'
+    ]; 
+
+    if(!empty($emailExists)) {
+      $params['message'] = 'This email adress is already taken'; 
+    }
+
+
+    //Sugeneruojame alerta jei ID jau yra uzimtas
+    if(array_key_exists($_POST['id'], $data) OR !empty($emailExists)) {
+
+      header('Location: ?' . http_build_query($params));
+      exit;
+    }
+    
+    $_POST['created_at'] = date('Y-m-d h:i:s');
+
+    $_POST['password'] = md5($_POST['password']);
+    $data[$_POST['id']] = $_POST;
+
+    file_put_contents('./database.json', json_encode($data));
+
+    $_SESSION['user'] = $_POST;
+
+    header('Location: ?page=/');
+
+
+  }
+?>
+
+
+<h1>Registration</h1>
+
+<form method="POST">
+  <div class="mb-3">
+    <label>Email</label>
+    <input type="email" name="email" placeholder="test@gmail.com" class="form-control" required />
+  </div>
+  <div class="mb-3">
+    <label>Password</label>
+    <input type="password" name="password" class="form-control" required />
+  </div>
+  <div class="mb-3">
+    <label>First name</label>
+    <input type="text" name="first_name" class="form-control" required />
+  </div>
+  <div class="mb-3">
+    <label>Last name</label>
+    <input type="text" name="last_name" class="form-control" required />
+  </div>
+  <div class="mb-3">
+    <label>Select ID</label>
+    <input type="password" name="id" placeholder="@" class="form-control" required />
+  </div>
+  <button class="btn btn-primary">Register</button>
+</form>
