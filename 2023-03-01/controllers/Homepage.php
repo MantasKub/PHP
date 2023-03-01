44c0074ea4a9f3094508af25c11ca432b19e41ab
MantasKub <mkubiliunas@gmail.com>
2023-03-01 14:26:05 +0200

    Updated with working nav

---
+++ 2023-03-01/controllers/Homepage.php
@@ -0,0 +1,24 @@
+<?php
+
+class Homepage {
+  public static function index() {
+    $videos = new Videos();
+    $videos = $videos->getrecords();
+
+    $categories = new Categories();
+    $categories = $categories->getrecords();
+
+    include 'views/homepage.php';
+
+  }
+
+  public static function byCategory($id) {
+    $videos = new Videos();
+    $videos = $videos->categoryVideos($id);
+
+    $categories = new Categories();
+    $categories = $categories->getrecords();
+
+    include 'views/homepage.php';
+  }
+}
